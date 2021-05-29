d441a599eef2d700fd63afff95574d772e4d6df8
yeomjw0907 <74677213+yeomjw0907@users.noreply.github.com>
2021-05-29 16:39:56 +0900

    주피터 샘플

---
+++ Untitled.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a = 1
+
+
+# In[4]:
+
+
+b = 2
+
+
+# In[5]:
+
+
+c= a+b
+
+
+# In[6]:
+
+
+print(c)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
